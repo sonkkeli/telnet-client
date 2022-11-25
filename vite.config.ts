80ba965a945d04a899a5ead65362e9d1b5524807
Sonja Laurila <laurila@google.com>
2022-11-25 11:34:02 +0100

    Adjust vite.config.ts to generate an Isolated Web App

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import eslint from 'vite-plugin-eslint'
-import webbundle from 'rollup-plugin-webbundle'
+import eslint from "vite-plugin-eslint";
+import webbundle from "rollup-plugin-webbundle";
+import * as wbnSign from "wbn-sign";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
 export default {
   plugins: [
     eslint(),
     webbundle({
-      baseURL: '/',
-      static: { dir: 'public' },
-    })
-  ]
-}
+      baseURL: new wbnSign.WebBundleId(
+        wbnSign.parsePemKey(process.env.ED25519KEY)
+      ).serializeWithIsolatedWebAppOrigin(),
+      static: { dir: "public" },
+      output: "signed.swbn",
+      integrityBlockSign: {
+        key: process.env.ED25519KEY,
+      },
+    }),
+  ],
+};
